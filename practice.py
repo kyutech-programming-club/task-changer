71cda4eba508c9261898ea32d783cee6f3d6c5c4
kei1115 <keifukuda1115@gmail.com>
2021-09-04 12:51:35 +0900

    Notificate when flame exceeds 30

--- practice.py
+++ practice.py
@@ -1,5 +1,6 @@
 import cv2
 import winsound
+from plyer import notification
 
 if __name__ == '__main__':
     # 定数定義
@@ -27,8 +28,15 @@ if __name__ == '__main__':
     cv2.namedWindow(ORG_WINDOW_NAME)
     cv2.namedWindow(GAUSSIAN_WINDOW_NAME)
 
+    flame = 0
+
     # 変換処理ループ
     while end_flag == True:
+
+        # 顔を認識しているかどうか
+        isFace = False
+        if (flame > 30):
+            flame = 0
 
         # 画像の取得と顔の検出
         img = c_frame
@@ -38,11 +46,23 @@ if __name__ == '__main__':
 
         # 検出した顔に印を付ける
         for (x, y, w, h) in face_list:
-            winsound.Beep(60, 3) 
-            color = (225, 0, 225)
+            isFace = True
+            flame = 0
+            color = (0, 0, 225)
             pen_w = 3
             cv2.rectangle(img_gray, (x, y), (x+w, y+h), color, thickness = pen_w)
 
+        # 顔を検出しなくなった時にビーブ音を鳴らす
+        if (isFace == False):
+            flame += 1
+        
+        if (flame > 30):
+            notification.notify(
+                title = "デスクトップ通知",
+                message = "これはテストです。通知が表示されます。",
+                timeout = 5
+            )
+        
         # フレーム表示
         cv2.imshow(ORG_WINDOW_NAME, c_frame)
         cv2.imshow(GAUSSIAN_WINDOW_NAME, img_gray)
